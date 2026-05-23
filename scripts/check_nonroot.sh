16c29ad49125eead8b9fd9fd7ef55b225119fb27
syedramim <parthib004@gmail.com>
2026-05-23 10:07:44 -0600

    added fixes for nonroot check script

--- scripts/check_nonroot.sh
+++ scripts/check_nonroot.sh
@@ -5,13 +5,13 @@ echo "Checking if USER is nonroot..."
 
 docker run -d --name nonroot_check secure_ci_image
 
-USERID=docker exec nonroot_check id
-
-if [ $USERID == 0 ]; then
-    echo "ERROR: Container is running as root. Failing build."
-    return 1
-else
-    echo "Container is running as: ${USERID}(nonroot)\nBuild ok."
-
+USERID=$(docker exec nonroot_check id -u)
 
 docker rm -f nonroot_check
+
+if [ $USERID -eq 0 ]; then
+    echo "ERROR: Container is running as root. Failing build."
+    exit 1
+else
+    echo "Container is running as: ${USERID}(nonroot)\nBuild ok."
+fi
