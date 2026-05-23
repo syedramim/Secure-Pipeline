2260829054b66b8737d6d33b7b7aadf131237740
syedramim <parthib004@gmail.com>
2026-05-23 10:12:31 -0600

    checking build with container as root

--- scripts/check_nonroot.sh
+++ scripts/check_nonroot.sh
@@ -10,7 +10,7 @@ USERID=$(docker exec nonroot_check id -u)
 docker rm -f nonroot_check
 
 if [ $USERID -eq 0 ]; then
-    echo "ERROR: Container is running as root. Failing build."
+    echo "ERROR: Container is running as root. Failing build." >&2
     exit 1
 else
     echo -e "Container is running as: ${USERID}(nonroot).\nBuild ok."
